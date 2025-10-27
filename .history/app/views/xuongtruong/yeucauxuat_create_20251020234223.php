adaf306e225cd6d94ed16c14b33fb5a0fd353f6b
Tzanguyn <142522691+Tzanguyn@users.noreply.github.com>
2025-10-27 18:05:12 +0700

    cập nhật

--- .history/app/views/xuongtruong/yeucauxuat_create_20251020234223.php
+++
@@ -1,3 +0,0 @@
-<?php
-// View chuyển từ yeucauxuat/create sang xuongtruong/yeucauxuat_create
-include APP_PATH . '/views/yeucauxuat/create.php';
--- .history/app/views/xuongtruong/yeucauxuat_list_20251020234226.php
+++
@@ -1,3 +0,0 @@
-<?php
-// View chuyển từ yeucauxuat/list sang xuongtruong/yeucauxuat_list
-include APP_PATH . '/views/yeucauxuat/list.php';
